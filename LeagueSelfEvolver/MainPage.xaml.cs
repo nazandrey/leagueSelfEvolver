26e7a257966c94bd8dd68ad58ce810ea960afa28
Nazarov Andrey <nazandrbox@yandex.ru>
2017-11-14 13:41:05 +0300

    set multiline text in columns

--- LeagueSelfEvolver/MainPage.xaml.cs
+++ LeagueSelfEvolver/MainPage.xaml.cs
@@ -14,6 +14,7 @@ using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 using LeagueSelfEvolver.Model;
 using Windows.System;
+using Telerik.UI.Xaml.Controls.Grid;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -49,6 +50,18 @@ namespace LeagueSelfEvolver
                 Event eventItem = eventListGrid.SelectedItem as Event;
                 goalModel.RemoveRow(eventItem);
             };
+        }
+
+        private void Page_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            int GridBorderWidth = 6,
+                ColumnBorderWidth = 2,
+                columnBorderWidth = (eventListGrid.Columns.Count-1) * ColumnBorderWidth,
+                widthDelta = GridBorderWidth + columnBorderWidth;
+            foreach (DataGridColumn column in eventListGrid.Columns)
+            {
+                column.Width = (eventListGrid.ActualWidth - widthDelta) / eventListGrid.Columns.Count;
+            }
         }
     }
 }
