8fae55c57d87f3c06eebc1dfb1208734ca21470d
nazandrey <nazandrbox@yandex.ru>
2018-02-25 14:39:57 +0300

    delete unnecessary references from TagCountStatPage

--- LeagueSelfEvolver/TagCountStatPage.xaml.cs
+++ LeagueSelfEvolver/TagCountStatPage.xaml.cs
@@ -1,22 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+﻿using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
 using LeagueSelfEvolver.Model;
-using Windows.System;
 using Telerik.UI.Xaml.Controls.Grid;
-using Windows.UI.Core;
-using System.Collections.ObjectModel;
 
 // Документацию по шаблону элемента "Пустая страница" см. по адресу http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
